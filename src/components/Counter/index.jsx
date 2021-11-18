ff71e7a1aa9a4171326b8f3681f92578281af78e
morghull <morghull@gmail.com>
2021-11-18 09:30:49 +0200

    add counter component

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { step: 1 };
+  }
+  render() {
+    return <section>
+       
+    </section>;
+  }
+}
+
+export default Counter;
