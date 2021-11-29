9a02c923761d09a72e7482bbb30da9e79e29e5fa
morghull <morghull@gmail.com>
2021-11-29 21:08:01 +0200

    add config to Counter

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Core from './Core';
 import StepTool from './StepTools';
 import styles from './Counter.module.css';
+import config from './config';
+const { defaultStepSize } = config;
 
 class Counter extends Component {
   constructor(props) {
     super(props);
-    this.state = { step: 1 };
+    this.state = { step: defaultStepSize };
   }
   setStep = (newStep) => {
     this.setState({ step: newStep });
@@ -16,10 +18,7 @@ class Counter extends Component {
     return (
       <section className={styles.section}>
         <Core step={step} />
-        <StepTool
-          step={step}
-          setStep={this.setStep}
-        />
+        <StepTool step={step} setStep={this.setStep} />
       </section>
     );
   }
